8479ef8c0dc17d1b10f7f258f2f52ce5f2e98fad
orafeal <orafeal@gmail.com>
2014-11-28 12:58:56 +0000

    nill

--- plugin.video.dfmalaystream/default.py
+++ plugin.video.dfmalaystream/default.py
@@ -159,11 +159,11 @@ def Dfm2uMenu():
         addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/dfm2u/movie/',
                              'startPage': '1', 'numOfPages': '1'}, {'title':  'DFM2U Movies'}, img=IconPath + 'dfm2uico.png', fanart=FanartPath + 'fanart.png')
         addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/dfm2u/telemovie/',
-                             'startPage': '1', 'numOfPages': '1'}, {'title':  'DFM2U Telemovies'}, img=IconPath + 'dfm2uico.png', fanart=FanartPath + 'fanart.png')
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'DFM2U Telemovie'}, img=IconPath + 'dfm2uico.png', fanart=FanartPath + 'fanart.png')
         addon.add_directory({'mode': 'GetEpisode', 'section': 'ALL', 'url': BASE_URL + '/dfm2u/drama/',
-                             'startPage': '1', 'numOfPages': '1'}, {'title':  'DFM2U Drama'}, img=IconPath + 'dfm2uico.png', fanart=FanartPath + 'fanart.png')
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'DFM2U Dramas'}, img=IconPath + 'dfm2uico.png', fanart=FanartPath + 'fanart.png')
         addon.add_directory({'mode': 'GetEpisode', 'section': 'ALL', 'url': BASE_URL + '/dfm2u/tvshow/',
-                             'startPage': '1', 'numOfPages': '1'}, {'title':  'DFM2U TV Shows'}, img=IconPath + 'dfm2uico.png', fanart=FanartPath + 'fanart.png')                                          
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'DFM2U TV Show'}, img=IconPath + 'dfm2uico.png', fanart=FanartPath + 'fanart.png')                                          
         xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------#
